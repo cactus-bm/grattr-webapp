3bb9835440d15325cec2769a914458f428bf522b
Amy Peniston <amy.peniston@gmail.com>
2020-08-22 09:11:26 -0300

    Added Sentry correct library

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route } from "react-router-dom"
 import styled from "@emotion/styled"
 import Home from "./components/Home"
 import Welcome from "./components/Welcome"
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/apm';
 
 Sentry.init({
   dsn: "https://bd5626ed0a9b48dc93c814ff67233ad1@o384166.ingest.sentry.io/5400416",
