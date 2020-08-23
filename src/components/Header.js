9f679e35e96edff736e69524032e6890a191eaf6
James Robinson <james@bermuda.earth>
2020-08-23 12:34:52 -0300

    Added Define Yourself headline.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,48 @@
-import React from 'react'
-import { connect } from "react-redux"
-import { signOutUser } from "../store/actions/authActions"
-import { Button, Grid, Box, Container } from "@material-ui/core"
+import React from "react";
+import { connect } from "react-redux";
+import { signOutUser } from "../store/actions/authActions";
+import { Button, Grid, Box, Container } from "@material-ui/core";
 
 const Header = ({ auth, signOut }) => {
-    const SignOutLink = () => {
-        if (auth.uid) {
-            return <Button variant="outlined" onClick={signOut} color="primary">Sign Out</Button>
-        }
-        else {
-            return <></>
-        }
+  const SignOutLink = () => {
+    if (auth.uid) {
+      return (
+        <Button variant="outlined" onClick={signOut} color="primary">
+          Sign Out
+        </Button>
+      );
+    } else {
+      return <></>;
     }
-    return (
-        <div style={{ backgroundColor: '#dddddd' }}>
-        <Container maxWidth="md" style={{ color: '#212121' }}>
-            <Grid container className="container" alignItems="center">
-                <Grid item xs={10}>
-                    <h1>Grattr</h1>      
-                </Grid>
-                <Grid item xs={2} >
-                    <Box mml={"auto"}> 
-                        <SignOutLink/>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Container>
-        </div>
-    )
-}
+  };
+  return (
+    <div style={{ backgroundColor: "#dddddd" }}>
+      <Container maxWidth="md" style={{ color: "#212121" }}>
+        <Grid container className="container" alignItems="center">
+          <Grid item xs={10}>
+            <h1>Grattr - Define Yourself</h1>
+          </Grid>
+          <Grid item xs={2}>
+            <Box mml={"auto"}>
+              <SignOutLink />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-        auth: state.firebaseAuth.auth
-    }
-}
+  return {
+    auth: state.firebaseAuth.auth,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        signOut: () => dispatch(signOutUser())
-    }
-}
+  return {
+    signOut: () => dispatch(signOutUser()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
